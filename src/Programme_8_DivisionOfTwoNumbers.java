89d9f36bf4e1250603254e682468c7009faff7f4
shweta <shwetasmit123@gmail.com>
2023-05-17 18:44:57 +0100

    My homework submitted

---
+++ src/Programme_8_DivisionOfTwoNumbers.java
@@ -0,0 +1,22 @@
+
+/*
+Write a Java Programme to divide two numbers and print on the screen*/
+/*Test data:50/3
+Expected Output :16
+ */
+public class Programme_8_DivisionOfTwoNumbers {
+    public static void main(String[] args) {
+        //The two integers are Value 1 and value2
+        int value1 = 50;
+        int value2 = 3;
+
+
+        int division = value1 / value2;
+        //concatenation
+        System.out.println("The " + value1 + " is divided by " + value2 + " the answer is " + division);
+    }
+}
+
+
+
+
